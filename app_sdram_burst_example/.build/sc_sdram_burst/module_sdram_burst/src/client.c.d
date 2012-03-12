92a88d9b961cfe9c10c54b0f8dee8676ce1d2672
Dave Lacey <david.lacey@xmos.com>
2012-03-12 11:41:54 +0000

    Updated infrastructure and metainformation

--- app_sdram_burst_example/.build/sc_sdram_burst/module_sdram_burst/src/client.c.d
+++
@@ -1,1 +0,0 @@
-.build/sc_sdram_burst/module_sdram_burst/src/client.c.d ./.build/sc_sdram_burst/module_sdram_burst/src/client.c.o: ./../../sc_sdram_burst/module_sdram_burst/src/client.c
--- app_sdram_burst_example/.build/sc_sdram_burst/module_sdram_burst/src/sdram_burst.xc.d
+++
@@ -1,3 +0,0 @@
-.build/sc_sdram_burst/module_sdram_burst/src/sdram_burst.xc.d ./.build/sc_sdram_burst/module_sdram_burst/src/sdram_burst.xc.o: ./../../sc_sdram_burst/module_sdram_burst/src/sdram_burst.xc \
-  ../../sc_sdram_burst/module_sdram_burst/src/sdram_burst.h
-
